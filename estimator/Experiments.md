e514d1f1bdfb0a7d186b38aa4b2947cb652e7290
Calvin Krist <calvin.krist@yahoo.com>
2021-04-24 12:57:06 -0400

    Add basic experiments script

---
+++ estimator/Experiments.md
@@ -0,0 +1,30 @@
+## Baselines
+
+Each job is un in full (500 epochs) 10 times on GPU and CPU each to get estimates for how long the jobs should take. The results are stored in `results/baselines.json` in the following format: 
+
+```
+{
+	"googleNet": {
+		"cpu" : [t1, t2, ..., t10],
+		"gpu" : [t1, t2, ..., t10]
+	}, 
+	"alexNet": {
+		"cpu" : [t1, t2, ..., t10],
+		"gpu" : [t1, t2, ..., t10]
+	}, 
+	"leNet": {
+		"cpu" : [t1, t2, ..., t10],
+		"gpu" : [t1, t2, ..., t10]
+	}, 
+}
+``` 
+
+## Linear Regression
+
+All of the linear regression results are stored in `results/linearRegression`, with further subdirectories for each job type, ie `results/linearRegression/googleNet`. Within each job type, there are again subdirectories for each configuration:
+
+* (0.6, 0.9): `results/linearRegression/JOB/config_0`
+* (0.4, 1.1): `results/linearRegression/JOB/config_1`
+* (0.2, 1.3): `results/linearRegression/JOB/config_2`
+
+For each configuration for each job, each job type was estimated 100 times with for CPU and GPU esimations. These estimations are saved as `estimation_i.json`. The format of these files is JSON, and were created by writting to file `json.dumps(job.__dict__)`, which stores a dictionary representation of the estimated job. Thus, it can be loaded in to a dictionary in Python and the meaningful properties -- `cpu_compl_time` and `gpu_compl_time` -- can be easily accessed.
--- estimator/main.py
+++ estimator/main.py
@@ -1,6 +1,102 @@
 from estimator import *
 import os
+import time
+import json
+
+def get_baselines():
+	print("Calculating baselines")
+	# Run each job in full 10 times, record the runtime
+	baselines = {"googleNet" : {"cpu" : [], "gpu" : []}, "alexNet" : {"cpu" : [], "gpu" : []}, "leNet" : {"cpu" : [], "gpu" : []}}
+	for i in range(10):
+		googleNet = GoogleNetJob(epochs=500)
+		alexNet = AlexNetJob(epochs=500)
+		leNet = LeNetJob(epochs=500)
+
+		print("Calculating googleNet baseline " + str(i))
+		start = time.perf_counter()
+		googleNet.run_cpu()
+		end = time.perf_counter()
+		baselines["googleNet"]["cpu"].append(end - start)
+
+		start = time.perf_counter()
+		googleNet.run_gpu()
+		end = time.perf_counter()
+		baselines["googleNet"]["gpu"].append(end - start)
+
+		print("Calculating alexNet baseline " + str(i))
+		start = time.perf_counter()
+		alexNet.run_cpu()
+		end = time.perf_counter()
+		baselines["alexNet"]["cpu"].append(end - start)
+
+		start = time.perf_counter()
+		alexNet.run_gpu()
+		end = time.perf_counter()
+		baselines["alexNet"]["gpu"].append(end - start)
+
+		print("Calculating leNet baseline " + str(i))
+		start = time.perf_counter()
+		leNet.run_cpu()
+		end = time.perf_counter()
+		baselines["leNet"]["cpu"].append(end - start)
+
+		start = time.perf_counter()
+		leNet.run_gpu()
+		end = time.perf_counter()
+		baselines["leNet"]["gpu"].append(end - start)
+
+	return baselines
 
 if __name__ == '__main__':
-	# Get baseline estimate for job runtimes
-    job = GoogleNetJob()
+	##############################################
+	####    Get baselines and save to JSON    ####
+	##############################################
+
+	if not os.path.exists('results'):
+		os.makedirs('results')
+
+	baselines = get_baselines()
+	with open('results/baselines.json', 'w') as outfile:
+		json.dump(baselines, outfile)
+
+	###############################
+	####    Run experiments    ####
+	###############################
+	estimation_methods = ["linearRegression", "timeWritter"]
+	for estimation_name in estimation_methods:
+		if not os.path.exists('results/' + estimation_name):
+			os.makedirs('results/' + estimation_name)
+
+		job_types = {"googleNet" : GoogleNetJob, "alexNet" : AlexNetJob, "leNet" : LeNetJob}
+		configurations = {"linearRegression" : [(0.6, 0.9), (0.4, 1.1), (0.2, 1.3)], "timeWritter" : [1, 2, 4, 7]}
+
+		for job_type, job_class in job_types.items():
+			if not os.path.exists('results/' + estimation_name + '/' + job_type):
+				os.makedirs('results/' + estimation_name + '/' + job_type)
+
+			for i, config in enumerate(configurations[estimation_name]):
+				file_path = ""
+				if estimation_name is "linearRegression":
+					if not os.path.exists('results/' + estimation_name + '/' + job_type + "/config_" + str(i)):
+						os.makedirs('results/' + estimation_name +'/' + job_type + "/config_" + str(i))
+					file_path = 'results/' + estimation_name + '/' + job_type + "/config_" + str(i)
+				elif estimation_name is "timeWritter":
+					if not os.path.exists('results/' + estimation_name + '/' + job_type + "/config_" + str(config)):
+						os.makedirs('results/' + estimation_name +'/' + job_type + "/config_" + str(config))
+					file_path = 'results/' + estimation_name + '/' + job_type + "/config_" + str(config)
+
+				# Estimate each job type at each configuration at each estimation method 100 times
+				for itr in range(100):
+					print("Estimating " + file_path)
+
+					job = job_class(epochs=500)
+					
+					if estimation_name is "linearRegression":
+						estimate_job_time_linreg(job, config[0], config[1])
+					elif estimation_name is "timeWritter":
+						estimate_job_time_writter(job, config)
+		
+					jsonStr = json.dumps(job.__dict__)
+					file_name = file_path + "/estimation_" + str(itr) + ".json"
+					with open(file_name, "w") as f:
+						f.write(jsonStr)
